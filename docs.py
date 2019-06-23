36e03d84ba75bfc13d5d2f58786d881b46a642c8
Marco Rougeth <marco@rougeth.com>
2019-06-23 15:34:21 +0100

    Create report analysis job

--- docs.py
+++ docs.py
@@ -12,6 +12,23 @@ import requests
 import schedule
 from decouple import config
 from loguru import logger
+from aiogram import Bot
+
+
+STATS_FIELDS = [
+    "completed",
+    "reviewed",
+    "reviewed_percentage",
+    "translated_entities",
+    "translated_words",
+    "untranslated_entities",
+    "untranslated_words",
+]
+
+STATS_MESSAGES = {
+    "reviewed": "- Traduções revisadas: +{}\n",
+    "translated_words": "- Palavras traduzidas: +{}\n",
+}
 
 
 class Transifex:
@@ -38,19 +55,11 @@ class Transifex:
         return resource, response
 
     async def stats(self, resources):
-        fields = [
-            "reviewed",
-            "translated_entities",
-            "translated_words",
-            "untranslated_entities",
-            "untranslated_words",
-        ]
-
         response = await asyncio.gather(
             *[self.resource_stat(resource) for resource in resources]
         )
 
-        stats = defaultdict(lambda: {field: 0 for field in fields})
+        stats = defaultdict(lambda: {field: 0 for field in STATS_FIELDS})
         for resource, stat in response:
             # Group stats considering the first part of slug.
             # Transform "c-api--abstract" and "c-api--allocation" into "c-api".
@@ -86,9 +95,100 @@ def run_daily_stats():
     loop.run_until_complete(daily_stats(output))
 
 
+def select_report_files(output):
+    stats_files = sorted(os.listdir(output), reverse=True)
+    latest_file = output / stats_files[0]
+
+    if len(stats_files) > 7:
+        previous_file = output / stats_files[6]
+    else:
+        previous_file = output / stats_files[-1]
+
+    logger.debug(
+        "Report files selected, latest_file={}, previous_file={}",
+        latest_file,
+        previous_file,
+    )
+
+    with latest_file.open() as latest_file, previous_file.open() as previous_file:
+        return json.load(latest_file), json.load(previous_file)
+
+
+def serialize_report(report, group_resources=False):
+    updated_report = defaultdict(lambda: {field: 0 for field in STATS_FIELDS})
+
+    for resource, stats in report.items():
+        if group_resources:
+            resource = resource.split("--")[0]
+
+        for field in stats:
+            updated_report[resource][field] += stats[field]
+
+    if updated_report.get("glossary_"):
+        updated_report["glossary"] = updated_report.pop("glossary_")
+
+    return updated_report
+
+
+def compare_reports():
+    output = Path(config("OUTPUT_DATA"))
+    reports = select_report_files(output)
+    latest_report, previous_report = map(
+        lambda r: serialize_report(r, group_resources=True), reports
+    )
+
+    report = defaultdict(dict)
+    for resource, stats in latest_report.items():
+        for stat, value in stats.items():
+            previous_value = previous_report[resource][stat]
+            if value != previous_value:
+                report[resource][stat] = value - previous_value
+
+    return report
+
+
+async def report():
+    bot = Bot(token=config("TELEGRAM_API_TOKEN"))
+    report = compare_reports()
+
+    # Consider only reviewed and words translated statistics
+    for resource in report:
+        report[resource] = {
+            k: v
+            for k, v in report[resource].items()
+            if k in ["reviewed", "translated_words"]
+        }
+
+    if not report:
+        logger.warning("Nothing to report!")
+        return
+
+    message = "📈 *Estastísticas da tradução*\nPeríodo: 7 dias\n\n"
+
+    for resource, stats in sorted(report.items()):
+        if not stats:
+            continue
+
+        message += f"*{resource}*\n"
+        for stat, value in stats.items():
+            stat_message = STATS_MESSAGES.get(stat)
+            if not stat_message:
+                continue
+            message += stat_message.format(value)
+
+    users = config(
+        "BROADCAST_REPORT_TO", cast=lambda v: [s.strip() for s in v.split(",")]
+    )
+
+    await asyncio.gather(*[
+        bot.send_message(user, message, parse_mode="Markdown")
+        for user in users
+    ])
+
+
 def main():
-    job = schedule.every().day.at("00:00").do(run_daily_stats)
-    job.run()
+    schedule.every().sunday.at("18:00").do(asyncio.run(report()))
+    schedule.every().day.at("00:00").do(run_daily_stats).run()
     while True:
         schedule.run_pending()
         time.sleep(1)
